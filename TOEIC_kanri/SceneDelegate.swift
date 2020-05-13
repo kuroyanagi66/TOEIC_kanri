6aaf547c1258c351214f811206805d2b8835e9e6
kuroyanagi66 <56671239+kuroyanagi66@users.noreply.github.com>
2020-05-13 11:26:34 +0900

    reset all 追加

--- TOEIC_kanri/SceneDelegate.swift
+++ TOEIC_kanri/SceneDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import SwiftUI
+import Combine
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -24,12 +25,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
         // Create the SwiftUI view and set the context as the value for the managedObjectContext environment keyPath.
         // Add `@Environment(\.managedObjectContext)` in the views that will need the context.
-        let contentView = ContentView().environment(\.managedObjectContext, context)
+        let setumon = Setumon()
+        let contentView = ContentView().environment(\.managedObjectContext, context).environmentObject(setumon)
+        
 
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
-            window.rootViewController = UIHostingController(rootView: contentView)
+            window.rootViewController = UIHostingController(rootView: contentView.environmentObject(TimerHolder()))
+
             self.window = window
             window.makeKeyAndVisible()
         }
